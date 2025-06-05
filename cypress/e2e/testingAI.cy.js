873d9c01b7acd5b6216a7ef8ce5fb2e9c5ea577d
Khodyachiy <n.khodyachiy@gmail.com>
2025-06-05 14:49:32 +0300

    Intercepting and modifying the request and response

--- cypress/e2e/testingAI.cy.js
+++ cypress/e2e/testingAI.cy.js
@@ -6,7 +6,7 @@ describe('Test with backend', () => {
   })
 
  
-  it.only('verify correct request and response', () => {
+  it('verify correct request and response', () => {
     // Intercepting the request to create an article
     cy.intercept('POST', /\/api\/articles\//).as('postArticles');
 
@@ -37,5 +37,31 @@ describe('Test with backend', () => {
       expect(xhr.response.body.article).to.have.property('description', 'This is a description');
     })
   })
+
+  it.only('intercepting and modifying the request and response', () => {
+    cy.intercept('POST', '**/articles', (req) => {
+      expect(req.body.article.description).to.equal('This is a description');
+      req.reply((res) => {
+        console.log('Response body:', res.body); // log for answer
+        if (res.body && res.body.article) {
+          res.body.article.description = "This is a description 2";
+        } else {
+          throw new Error('Response body or article is undefined');
+        }
+      });
+    }).as('postArticles');  
+
+    cy.contains('New Article').click();
+    cy.get('[formcontrolname="title"]').type('This is the title');
+    cy.get('[formcontrolname="description"]').type('This is a description');
+    cy.get('[formcontrolname="body"]').type('This is a body of the Article');
+    cy.contains('Publish Article').click();
+
+    cy.wait('@postArticles').then((xhr) => {
+      expect(xhr.response.statusCode).to.equal(201); // or 200, depended by API
+      expect(xhr.request.body.article.body).to.equal('This is a body of the Article');
+      expect(xhr.response.body.article.description).to.equal('This is a description 2');
+    });
+  });
 
 })
